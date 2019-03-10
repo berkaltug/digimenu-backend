7c5e1dd7e0923995072f964b0aeebfb5c538dbb5
Berk Altuğ <berkaltug96@gmail.com>
2019-03-10 11:46:50 +0300

    res login

--- src/main/java/com/digimenu/main/controller/RestaurantController.java
+++ src/main/java/com/digimenu/main/controller/RestaurantController.java
@@ -1,23 +1,50 @@
 package com.digimenu.main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.digimenu.main.dto.RestaurantDto;
+import com.digimenu.main.service.CustomUserDetailsService;
 
 @Controller
 @RequestMapping(value="/restaurant")
 public class RestaurantController {
-
-	@GetMapping("/login")
-	public String getlogin() {
-		return "login";
-	}
 	
+	@Autowired 
+	CustomUserDetailsService userDetailsService;
+	
+	@RequestMapping(value="/login",method=RequestMethod.GET)
+	public ModelAndView getlogin() {
+		ModelAndView mav = new ModelAndView();
+	    mav.setViewName("login");
+	    return mav;
+	}
+//	@RequestMapping(value="/login" ,method=RequestMethod.POST)
+//	public String postlogin(@ModelAttribute("restaurantDto") RestaurantDto resDto,Model model) {
+//		
+//		Authentication authentication=SecurityContextHolder.getContext().getAuthentication(); //kullanmadık
+//		UserDetails userDetails=null;
+//		
+//		
+//		if( userDetails!=null ) {
+//			return "restaurant/showtable";
+//		}
+//	}
 	@GetMapping("/login-error")
 	public String errorlogin(Model model) {
-		model.addAttribute("loginError", true);
+		 model.addAttribute("loginError", true);
 		return "login";
 	}
 	
---
+++ src/main/java/com/digimenu/main/dto/RestaurantDto.java
@@ -0,0 +1,30 @@
+package com.digimenu.main.dto;
+
+import org.springframework.stereotype.Component;
+
+@Component("restaurantDto")
+public class RestaurantDto {
+	
+	private String username;
+	private String password;
+	public RestaurantDto(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	public RestaurantDto() {
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
