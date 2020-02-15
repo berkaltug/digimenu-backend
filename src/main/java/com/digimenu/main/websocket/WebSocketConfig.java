2e7e66410c59c36692dd1b325a5fb1bc8ce1c7e1
berkaltug96@gmail.com <berkaltug96@gmail.com>
2020-02-16 00:52:35 +0300

    tries socket.onclose

--- src/main/java/com/digimenu/main/websocket/WebSocketConfig.java
+++ src/main/java/com/digimenu/main/websocket/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.digimenu.main.websocket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -17,7 +20,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/restaurant/message");
-		config.setApplicationDestinationPrefixes("/app"); //@MessageMapping kullandığım zaman işe yarar yani client-to-sv msg
+		config
+			.enableSimpleBroker("/restaurant/message")
+			.setTaskScheduler(heartBeatScheduler());
+		config
+			.setApplicationDestinationPrefixes("/app"); //@MessageMapping kullandığım zaman işe yarar yani client-to-sv msg
+	}
+
+	@Bean
+	public TaskScheduler heartBeatScheduler(){
+		return new ThreadPoolTaskScheduler();
 	}
 }
